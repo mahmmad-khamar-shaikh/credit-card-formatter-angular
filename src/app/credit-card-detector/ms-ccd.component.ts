a98e5c6bda0eb028e665dbd6b6eed421d3f896d1
M K Shaikh <shahid13384@hotmail.com>
2017-09-10 11:52:14 +0530

    first commit

---
+++ src/app/credit-card-detector/ms-ccd.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'ms-ccd',
+    templateUrl: './ms-ccd.component.html',
+    styleUrls : ['./ms-ccd.component.css']
+})
+export class CardDetectorComponent implements OnInit {
+    ngOnInit() {
+
+    }
+}
